ad3cb0353256fdf7c1bb5b14d1023b7fa7687924
lhartley870 <hartleylc@hotmail.co.uk>
2021-09-07 19:19:52 +0000

    Add get_horizontal_ship_start method to Ship parent class to select a random grid starting column position for horizontal ships

--- run.py
+++ run.py
@@ -9,3 +9,9 @@ class Ship:
         self.length = length
 
     orientation = random.choice(('Horizontal', 'Vertical'))
+
+    def get_horizontal_ship_start(self):
+        all_columns = [x for x in range(1, 20)]
+        odd_columns = [x for x in all_columns if x % 2 != 0]
+        column_start_position = random.choice(odd_columns)
+        return column_start_position
