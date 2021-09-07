39546f5cbdbdf22e3d94a375787914a717617601
lhartley870 <hartleylc@hotmail.co.uk>
2021-09-07 20:00:00 +0000

    Ship class: amend column method and add row method

--- run.py
+++ run.py
@@ -10,19 +10,23 @@ class Ship:
 
     orientation = random.choice(('Horizontal', 'Vertical'))
 
-    def get_horizontal_ship_start(self):
+    def get_board_column(self):
         all_columns = [x for x in range(1, 20)]
         odd_columns = [x for x in all_columns if x % 2 != 0]
-        column_start_position = random.choice(odd_columns)
-        return column_start_position
+        chosen_column = random.choice(odd_columns)
+        return chosen_column
+
+    def get_board_row(self):
+        chosen_row = random.randrange(1, 11)
+        return chosen_row
 
     def get_ship_position(self):
         if self.orientation == 'Horizontal':
-            row = random.randrange(1, 11)
-            column_start = self.get_horizontal_ship_start()
-            if (column_start - 2) + self.length * 2 > 19:
+            row = self.get_board_row()
+            starting_column = self.get_board_column()
+            if (starting_column - 2) + self.length * 2 > 19:
                 return self.get_ship_position()
             else:
-                return row, column_start
+                return row, starting_column
         else:
             return ('for vertical ships')
