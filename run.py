c3a345b8f0c4349100e4320a3e06f73ef9960e67
lhartley870 <hartleylc@hotmail.co.uk>
2021-09-09 10:46:26 +0000

    Ship class: add docstrings and comments to explain class & class methods

--- run.py
+++ run.py
@@ -5,16 +5,35 @@ import random
 
 
 class Ship:
+    """
+    Ship superclass. Sets the ship length. Has methods culminating
+    in a final get_board_positions method which generates the board
+    coordinates for the ship.
+    """
     def __init__(self, length):
+        """
+        Creates an instance of Ship with the instance attribute length.
+        """
         self.length = length
 
     orientation = random.choice(('Horizontal', 'Vertical'))
 
     def get_board_row_or_column(self):
+        """
+        Method to generate a board row or column number
+        used by the get_ship_position method.
+        """
         chosen_row_or_column = random.randrange(1, 11)
         return chosen_row_or_column
 
     def get_ship_position(self):
+        """
+        Method to generate a board row and starting column number
+        for horizontally oriented ships and a board column and starting
+        row number for vertically oriented ships used by the
+        get_board_positions method.
+        """
+        # If statement applies where the ship is horizontally oriented.
         if self.orientation == 'Horizontal':
             row = self.get_board_row_or_column()
             starting_column = self.get_board_row_or_column()
@@ -22,6 +41,7 @@ class Ship:
                 return self.get_ship_position()
             else:
                 return self.orientation, row, starting_column
+        # Else statement applies where the ship is vertically oriented.
         else:
             column = self.get_board_row_or_column()
             starting_row = self.get_board_row_or_column()
@@ -31,9 +51,17 @@ class Ship:
                 return self.orientation, starting_row, column
 
     def get_board_positions(self):
+        """
+        Method that uses the ship orientation, board row and starting column
+        for horizontally oriented ships and the ship orientation, board column
+        and starting row for vertically oriented ships returned by the
+        get_ship_position method, along with the ship's length, to generate
+        a list of board coordinates for the ship.
+        """
         ship_position = self.get_ship_position()
         orientation, row, column = ship_position
         board_coordinates = []
+        # If statement applies where the ship is horizontally oriented.
         if orientation == 'Horizontal':
             column_positions = [column]
             for ind in range(self.length - 1):
@@ -45,6 +73,7 @@ class Ship:
                 board_coordinate.append(row)
                 board_coordinate.append(column_position)
                 board_coordinates.append(board_coordinate)
+        # Else statement applies where the ship is vertically oriented.
         else:
             row_positions = [row]
             for ind in range(self.length - 1):
