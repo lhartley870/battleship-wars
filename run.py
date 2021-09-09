43733694705e2b535747cc193e4d411f05e73be4
lhartley870 <hartleylc@hotmail.co.uk>
2021-09-09 10:27:31 +0000

    Ship class: consolidate methods for generating column & row numbers & amend other class methods accordingly

--- run.py
+++ run.py
@@ -10,27 +10,21 @@ class Ship:
 
     orientation = random.choice(('Horizontal', 'Vertical'))
 
-    def get_board_column(self):
-        all_columns = [x for x in range(1, 20)]
-        odd_columns = [x for x in all_columns if x % 2 != 0]
-        chosen_column = random.choice(odd_columns)
-        return chosen_column
-
-    def get_board_row(self):
-        chosen_row = random.randrange(1, 11)
-        return chosen_row
+    def get_board_row_or_column(self):
+        chosen_row_or_column = random.randrange(1, 11)
+        return chosen_row_or_column
 
     def get_ship_position(self):
         if self.orientation == 'Horizontal':
-            row = self.get_board_row()
-            starting_column = self.get_board_column()
-            if (starting_column - 2) + self.length * 2 > 19:
+            row = self.get_board_row_or_column()
+            starting_column = self.get_board_row_or_column()
+            if (starting_column - 1) + self.length > 10:
                 return self.get_ship_position()
             else:
                 return self.orientation, row, starting_column
         else:
-            column = self.get_board_column()
-            starting_row = self.get_board_row()
+            column = self.get_board_row_or_column()
+            starting_row = self.get_board_row_or_column()
             if (starting_row - 1) + self.length > 10:
                 return self.get_ship_position()
             else:
@@ -44,7 +38,7 @@ class Ship:
             column_positions = [column]
             for ind in range(self.length - 1):
                 column_position = column_positions[ind]
-                column_position += 2
+                column_position += 1
                 column_positions.append(column_position)
             for column_position in column_positions:
                 board_coordinate = []
